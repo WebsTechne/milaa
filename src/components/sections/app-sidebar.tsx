e13a4090498146e23e0962dc00699bb6dde90713
Triumph <triumphaidenojie08@gmail.com>
2026-06-12 22:11:01 +0100

    feat: complete sign in page

--- src/components/sections/app-sidebar.tsx
+++ src/components/sections/app-sidebar.tsx
@@ -1,3 +1,31 @@
+import { Link } from "@tanstack/react-router"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "../ui/sidebar"
+
 export function AppSidebar() {
-  return <></>
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              className="data-[slot=sidebar-menu-button]:p-1.5!"
+              render={<Link to="/" />}
+            >
+              <span className="font-logo text-primary text-2xl font-extrabold lg:text-3xl">
+                mìlà
+              </span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent></SidebarContent>
+    </Sidebar>
+  )
 }
