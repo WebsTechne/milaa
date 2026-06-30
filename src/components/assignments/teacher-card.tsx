035dcdc5d5870af67504c3f52c8f7ad6d6c2d12a
Triumph <triumphaidenojie08@gmail.com>
2026-06-30 13:59:21 +0100

    chore: WIP - assignment card UI

--- src/components/assignments/teacher-card.tsx
+++ src/components/assignments/teacher-card.tsx
@@ -10,6 +10,14 @@ import {
 } from "../ui/card"
 import { cn } from "#/lib/utils"
 import { getDueLabel } from "#/lib/due-time"
+import { Button } from "../ui/button"
+import { IconCopy, IconDotsVertical } from "@tabler/icons-react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu"
 
 function TeacherAssignmentCard({
   assignment,
@@ -30,7 +38,41 @@ function TeacherAssignmentCard({
     <Link to="/assignments/$assignmentId" params={{ assignmentId: id }}>
       <Card>
         <CardHeader>
-          <CardTitle className="truncate">{title}</CardTitle>
+          <CardTitle className="flex-between items-start gap-1">
+            <span className="line-clamp-2">{title}</span>
+            <DropdownMenu>
+              <DropdownMenuTrigger
+                render={
+                  <Button
+                    size="icon-sm"
+                    variant="ghost"
+                    onClick={(e) => {
+                      e.preventDefault()
+                      e.stopPropagation()
+                    }}
+                  />
+                }
+              >
+                <IconDotsVertical className="size-5!" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="min-w-40">
+                <DropdownMenuItem>
+                  <IconCopy /> Copy code
+                </DropdownMenuItem>
+                {!isStudent && (
+                  <>
+                    <DropdownMenuItem>
+                      <Iconedit />
+                      Edit assignment
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>View submissions</DropdownMenuItem>
+                    {/*<Separator/>*/}
+                    <DropdownMenuItem>Delete assignment</DropdownMenuItem>
+                  </>
+                )}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </CardTitle>
           {description && (
             <CardDescription className="line-clamp-3">
               {description}
@@ -44,6 +86,7 @@ function TeacherAssignmentCard({
               {maxScore} mark{maxScore !== 1 && "s"}
             </span>
           </span>
+
           <span
             className={cn(
               "flex items-center gap-1 rounded-full border px-2 py-1 text-sm",
--- src/components/assignments/teachers.tsx
+++ src/components/assignments/teachers.tsx
@@ -7,16 +7,56 @@ import { Button } from "../ui/button"
 import { useState } from "react"
 import { NewAssignmentSheet } from "./new-assignment"
 import { IconPlus } from "@tabler/icons-react"
+import { useHeaderStore } from "#/lib/store"
+import { Link, useParams, useRouterState } from "@tanstack/react-router"
+import { Spinner } from "../ui/spinner"
 
 function TeacherAssignmentsPage() {
+  const routerState = useRouterState()
+
   const { data: session, isPending: authPending } = authClient.useSession()
   const queryClient = useQueryClient()
   const [newOpen, setNewOpen] = useState<boolean>(false)
+
+  const setTitleSlot = useHeaderStore((s) => s.setTitleSlot)
 
   const { data: assignments = [], isPending } = useQuery({
     queryKey: ["assignments", "list"],
     queryFn: () => getTeacherAssignments(),
   })
+
+  if (authPending || (!!session && isPending))
+    return (
+      <div className="flex-center h-[calc(100dvh-16px-48px)]">
+        <Spinner className="size-7" />
+      </div>
+    )
+
+  if (!session) {
+    setTitleSlot("Sign in required")
+
+    return (
+      <div className="flex-center h-[calc(100dvh-48px-56px)] px-5">
+        <div>
+          <h1 className="font-heading mb-2 text-4xl font-bold">
+            Sign in required
+          </h1>
+          <p className="text-muted-foreground max-w-prose text-base md:text-lg">
+            Sign in to continue. Once you're signed in, you'll be able to access
+            this page and everything it contains. Sign in at the{" "}
+            <Link
+              to="/auth/sign-in"
+              search={{ redirect: routerState.location.href }}
+              className="text-foreground! whitespace-nowrap underline underline-offset-4"
+            >
+              Sign in
+            </Link>{" "}
+            page
+          </p>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
